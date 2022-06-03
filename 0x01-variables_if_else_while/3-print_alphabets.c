9b427ee180a362859bad4adc631c23897a11def2
Zot0 <artlechoano@gmail.com>
2022-06-03 15:37:42 -0700

    Task 4 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+	putchar (ch++);
+	}
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+	putchar (ch++);
+	}
+	putchar ('\n');
+	return (0);
+}
