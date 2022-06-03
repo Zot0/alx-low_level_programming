bd28bf1e17e32d6e7e176f2af5d58d627f99eb06
Zot0 <artlechoano@gmail.com>
2022-06-02 17:42:49 -0700

    Task 5 Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+
+	return (0);
+
+}
