51dfd170addf4bd01610447ae4a0dfe45651c926
Zot0 <artlechoano@gmail.com>
2022-06-08 15:08:42 -0700

    Task 8 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,24 +9,13 @@
 
 int print_last_digit(int n)
 {
-	if (n < 10 && n > 0)
-	{
-		_putchar (n);
-		return (n);
-	}
-	else if (n < 0)
-	{
-		n = n * -1;
-		if (n < 10 && n > 0)
-		{
-			_putchar (n);
-			return (n);
-		}
-	}
-	else
-	{
-	_putchar ((n %  10) + '0');
-	}
+	int last = n % 10;
 
-	return (n % 10);
+	if (n < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+
+	return (last);
 }
