aa25262bff40a7039d0b6904208cc6a1b59e2028
Zot0 <artlechoano@gmail.com>
2022-06-08 10:39:33 -0700

    Task 2 alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
