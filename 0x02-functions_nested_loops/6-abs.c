dbd0d59d37fedb8fc0f15b16504ef5d1bdb9c538
Zot0 <artlechoano@gmail.com>
2022-06-08 13:16:29 -0700

    Task 7 absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value of an integer
+ *@n: int parameter
+ * Return: Absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	return (n);
+}
