bb0522f9ee51ee2becb60ec5e7ce721b2c4ebe1b
Zot0 <artlechoano@gmail.com>
2022-06-08 13:49:18 -0700

    Task 8 3 colors, 10 digits, and 7 notes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print last digit of a number
+ * @n: Value of digits
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	n % = 10;
+
+	if (n < 0)
+	{
+	n = -n
+	}
+	_putchar (n = '0');
+	return (n);
+
+}
