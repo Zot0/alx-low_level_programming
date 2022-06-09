4ae1040f288ba783082525fea795176576c3d777
Zot0 <artlechoano@gmail.com>
2022-06-09 15:40:54 -0700

    Task 1 isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ *@c: character for arguement
+ * Return: 1 or 0.
+ */
+int _isupper(int c);
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
