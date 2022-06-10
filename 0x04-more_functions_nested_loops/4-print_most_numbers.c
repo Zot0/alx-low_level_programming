ba1174ed46c2fc0aa0c88da587c0adca0da16667
Zot0 <artlechoano@gmail.com>
2022-06-09 17:21:48 -0700

    Task 5 numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_puthchar(i);
+		}
+		_putchar('\n');
+	}
+}
