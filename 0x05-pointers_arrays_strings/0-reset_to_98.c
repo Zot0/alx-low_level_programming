23eabbe8a6f683fcf1ce76ee581285815ee9e914
Zot0 <artlechoano@gmail.com>
2022-06-13 14:56:22 -0700

    Task 1 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - Updates the value pointed to
+ * @n: value of parameter
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
