d5eb61a8ec6829957672d78265319b7978bd19e0
Zot0 <artlechoano@gmail.com>
2022-06-14 15:50:51 -0700

    Task 3 This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - determine the length of string
+ * @s: String to be returned
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+}
