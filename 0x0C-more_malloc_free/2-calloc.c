290db80e0e11cec79ed47bb577a710e334162040
Zot0 <artlechoano@gmail.com>
2022-06-30 18:04:31 -0700

    Task 3 _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 
 /**
- * array_range - Allocates memory for an array
- * @min: number min
- * @max: number max
- * Return: a pointer to an array
+ * _calloc -  Allocates memory for an array
+ *@nmemb: Size of the array
+ *@size: Size of the type
+ * Return: Pointer to an array
  */
 
-int *array_range(int min, int max)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array = NULL;
-	int i, t, count;
+	void *array = NULL;
+	unsigned int i;
+	unsigned int t;
+	char *f;
 
-	count = 0;
-	if (min > max)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
-	t = (max - min) + 1;
-	array = malloc(t * sizeof(int));
-	if (array != NULL)
+	t = nmemb * size;
+	array = malloc(t);
+	f = (char *)array;
+	if (f != NULL)
 	{
-		for (i = min; i <= max; i++)
-		{
-			array[count] = i;
-			count++;
-		}
-		return (array);
+		for (i = 0; i < t; i++)
+			f[i] = 0;
+		return (f);
 	}
 	return (NULL);
 }
