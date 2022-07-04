2dc87e94b027a0f8f0c0eefcc285376f9190cc04
Zot0 <artlechoano@gmail.com>
2022-07-04 16:53:16 -0700

    Task 3 File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of the source code file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    printf("%s\n", __FILE__);
+
+    return 0;
+}
