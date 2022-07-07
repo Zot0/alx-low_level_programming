9cd9b0c644797ff2a76792d4b29d36dc9bfa13fe
Zot0 <artlechoano@gmail.com>
2022-07-06 20:28:07 -0700

    Task 1 What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - Print a name
@@ -10,8 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{
-		f(name);
-	}
+	f(name);
+
 }
