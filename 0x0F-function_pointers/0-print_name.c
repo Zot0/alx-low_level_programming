ee7cfeadf71d0c0a1191d163a6557603162c1647
Zot0 <artlechoano@gmail.com>
2022-07-06 20:06:03 -0700

    Task 1 What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name
+ * @name: String
+ * @f: Funtion pointer
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
