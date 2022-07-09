71d4fbd8089b777aab1c1f28f3989dbc4a1cbfdc
Zot0 <artlechoano@gmail.com>
2022-07-08 17:18:57 -0700

    Task 2 To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,14 +3,14 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings -Print strings
+ * print_numbers -Print numbers
  *@separator: Printed between numbers
  *@n: Number of arguments
  * Return: the sum of the arguments
  */
-void print_strings(const char *separator, const unsigned int n, ...)
-{
 
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
 	va_list li;
 	unsigned int i;
 
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(li, int));
 
 		if (i != (n - 1) && separator != NULL)
-			printf("%s", separator);
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(li);
